bce49077f3b679d54f2bf3d5abcfa610c3696eb4
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-08 17:17:10 -0500

    routing in next

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className="heading">Aprendiendo Next</h1>
+      <Link href="/nosotros">Ir a nosotros</Link>
     </div>
   );
 }
---
+++ pages/nosotros.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nosotros = () => {
+  return (
+    <div>
+      <h1>Desde Nosotros</h1>;<a href="/">ir a inicio</a>
+    </div>
+  );
+};
+
+export default Nosotros;
