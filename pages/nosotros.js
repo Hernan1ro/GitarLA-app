443a01b4b1c5a29e7a8e3754265dfb7b92c1115c
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-09 00:50:50 -0500

    creating about section

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,9 +1,40 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
+import styles from "../styles/Nosotros.module.css";
 
 const Nosotros = () => {
   return (
     <Layout pagina={"Nosotros"}>
-      <h1>Desde Nosotros</h1>
+      <main className="contenedor">
+        <h2 className="heading">Nosotros</h2>
+        <div className={styles.contenido}>
+          <Image
+            layout="responsive"
+            width={600}
+            height={450}
+            src="/img/nosotros.jpg"
+            alt="Imagen sobre nosotros"
+          />
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+              excepturi architecto deserunt minus cupiditate illum officia at
+              nobis laudantium vel sint aspernatur nihil porro neque accusantium
+              alias odit, ea, commodi quas! Laboriosam tenetur repellat est,
+              molestiae ea voluptates delectus ex porro, dolor, doloribus
+              repudiandae voluptatibus expedita suscipit praesentium ab officia?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+              excepturi architecto deserunt minus cupiditate illum officia at
+              nobis laudantium vel sint aspernatur nihil porro neque accusantium
+              alias odit, ea, commodi quas! Laboriosam tenetur repellat est,
+              molestiae ea voluptates delectus ex porro, dolor, doloribus
+              repudiandae voluptatibus expedita suscipit praesentium ab officia?
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 };
