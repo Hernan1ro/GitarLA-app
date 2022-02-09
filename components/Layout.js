39f6db3fd16e5181339ecb6e7d20f0c27adfc74e
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-08 22:45:42 -0500

    creating a layout component

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+const Layout = ({ children, pagina }) => {
+  return (
+    <div>
+      <Head>
+        <title>GuitarLA - {pagina}</title>
+        <meta name="description" content="Sitio Web de venta de guitarras" />
+      </Head>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Layout from "../components/Layout";
 
 const Nosotros = () => {
   return (
-    <div>
+    <Layout pagina={"Nosotros"}>
       <h1>Desde Nosotros</h1>;<a href="/">ir a inicio</a>
-    </div>
+    </Layout>
   );
 };
 
