f1a59d542fa82c9f19b9b6608200ee0f1d327754
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-08 23:39:23 -0500

    styling the header component

--- components/Header.js
+++ components/Header.js
@@ -7,15 +7,22 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className="contenedor">
-        <Link href="/">
-          <Image src="/img/logo.svg" width={400} height={100} alt="Page logo" />
-        </Link>
-        <nav className={styles.navegacion}>
-          <Link href="/">Inicio</Link>
-          <Link href="/nosotros">Nosotros</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/tienda">Tienda</Link>
-        </nav>
+        <div className={styles.barra}>
+          <Link href="/">
+            <Image
+              src="/img/logo.svg"
+              width={400}
+              height={100}
+              alt="Page logo"
+            />
+          </Link>
+          <nav className={styles.navegacion}>
+            <Link href="/">Inicio</Link>
+            <Link href="/nosotros">Nosotros</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/tienda">Tienda</Link>
+          </nav>
+        </div>
       </div>
     </header>
   );
