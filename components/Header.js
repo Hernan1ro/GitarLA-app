b08c775f8943dde6221aa1ea5a9754b7925cb7d8
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-08 23:24:25 -0500

    image next component

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className="contenedor">
-        <div></div>
+        <Link href="/">
+          <Image src="/img/logo.svg" width={400} height={100} alt="Page logo" />
+        </Link>
         <nav className={styles.navegacion}>
           <Link href="/">Inicio</Link>
           <Link href="/nosotros">Nosotros</Link>
