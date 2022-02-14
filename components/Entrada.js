38500fcdc2743595d00416e63a7c882edffc4378
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-14 16:29:43 -0500

    showing images in nextjs

--- components/Entrada.js
+++ components/Entrada.js
@@ -1,4 +1,4 @@
-import React from "react";
+import Image from "next/image";
 import Link from "next/link";
 
 const Entrada = ({ entrada }) => {
@@ -7,6 +7,13 @@ const Entrada = ({ entrada }) => {
 
   return (
     <article>
+      <Image
+        layout="responsive"
+        width={800}
+        height={600}
+        src={imagen}
+        alt={`imagen blog ${titulo}`}
+      />
       <div>
         <h1>{titulo}</h1>
         <p>{published_at}</p>
