73bf3a4e0258cddd2a29873b940b591838784cbd
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-15 08:20:43 -0500

    showing content in a dinamic page

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,11 +1,39 @@
+import { useEffect } from "react";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { entradas } from "../../API/blogsData.js";
+import Layout from "../../components/Layout";
 
 const EntradaBlog = () => {
-  const router = useRouter();
-  const entrada = entradas.filter((entrada) => entrada.id === router.query.id);
-  console.log(entrada);
-  return <div>EntradaBlog</div>;
+  const router = useRouter(); //obtener id de la id
+
+  return (
+    <Layout>
+      {entradas.map((entrada) => {
+        if (entrada.id === router.query.id) {
+          const { titulo, imagen, contenido, published_at, id } = entrada;
+          return (
+            <main key={id}>
+              <h1 className="heading">{titulo}</h1>
+              <article>
+                <img
+                  layout="responsive"
+                  width={800}
+                  height={600}
+                  src={imagen.src}
+                  alt={`Imagen entrada ${titulo}`}
+                />
+                <div>
+                  <p>{published_at}</p>
+                  <p>{contenido}</p>
+                </div>
+              </article>
+            </main>
+          );
+        }
+      })}
+    </Layout>
+  );
 };
 
 export default EntradaBlog;
