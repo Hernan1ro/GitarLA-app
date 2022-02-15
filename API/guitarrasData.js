a7d8acad4c065dbac2572f265903a13d5b3b89f5
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-15 13:45:17 -0500

    registering guitar data

--- API/guitarrasData.js
+++ API/guitarrasData.js
@@ -1,13 +1,112 @@
 import { v4 as uuidv4 } from "uuid";
 import guitarra_01 from "../public/img/guitarra_01.jpg";
+import guitarra_02 from "../public/img/guitarra_02.jpg";
+import guitarra_03 from "../public/img/guitarra_03.jpg";
+import guitarra_04 from "../public/img/guitarra_04.jpg";
+import guitarra_05 from "../public/img/guitarra_05.jpg";
+import guitarra_06 from "../public/img/guitarra_06.jpg";
+import guitarra_07 from "../public/img/guitarra_07.jpg";
+import guitarra_08 from "../public/img/guitarra_08.jpg";
+import guitarra_09 from "../public/img/guitarra_09.jpg";
+import guitarra_10 from "../public/img/guitarra_10.jpg";
+import guitarra_11 from "../public/img/guitarra_11.jpg";
+import guitarra_12 from "../public/img/guitarra_12.jpg";
 
-export const entradas = [
+export const guitarras = [
   {
     id: uuidv4(),
-    nombre: "Pasos para aprender a tocar guitarra",
+    nombre: "Cobain",
     descripcion:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut quo fugit, earum fugiat aliquid voluptatibus. Ducimus, porro? Maiores quos quasi alias in dolore.",
-    precio: 200,
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 359,
     imagen: guitarra_01,
+  },
+  {
+    id: uuidv4(),
+    nombre: "Dale",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 349,
+    imagen: guitarra_02,
+  },
+  {
+    id: uuidv4(),
+    nombre: "Borland",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 259,
+    imagen: guitarra_03,
+  },
+  {
+    id: uuidv4(),
+    nombre: "Campbell",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 379,
+    imagen: guitarra_04,
+  },
+  {
+    id: uuidv4(),
+    nombre: "Reed",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 299,
+    imagen: guitarra_05,
+  },
+  {
+    id: uuidv4(),
+    nombre: "Hazel",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 349,
+    imagen: guitarra_06,
+  },
+  {
+    id: uuidv4(),
+    nombre: "Lukather",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 349,
+    imagen: guitarra_07,
+  },
+  {
+    id: uuidv4(),
+    nombre: "Vai",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 299,
+    imagen: guitarra_08,
+  },
+  {
+    id: uuidv4(),
+    nombre: "Thompson",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 349,
+    imagen: guitarra_09,
+  },
+  {
+    id: uuidv4(),
+    nombre: "White",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 329,
+    imagen: guitarra_10,
+  },
+  {
+    id: uuidv4(),
+    nombre: "Krieger",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 359,
+    imagen: guitarra_11,
+  },
+  {
+    id: uuidv4(),
+    nombre: "Srv",
+    descripcion:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum perspiciatis distinctio consequatur recusandae quaerat soluta nemo ullam earum animi debitis molestias, asperiores similique corporis quas!",
+    precio: 349,
+    imagen: guitarra_12,
   },
 ];
