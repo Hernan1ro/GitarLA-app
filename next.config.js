8a04ced137e8b2c97384812dc71634d2ea8fc5a0
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-16 22:09:56 -0500

    allowing netlify access to the next image component

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "https://www.netlify.com/",
+      "https://620db9dc8b07cf0008f1aba5--naughty-shannon-c31a6b.netlify.app/",
+      ,
+      "https://app.netlify.com/",
+    ],
+  },
+};
